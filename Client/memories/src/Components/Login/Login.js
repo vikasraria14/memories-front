9d15e9971338c3e8bc0b767a290a3494f4a80cae
vikasraria14 <38049874+vikasraria14@users.noreply.github.com>
2022-09-11 01:40:22 +0530

    Working fine

--- Client/memories/src/Components/Login/Login.js
+++ Client/memories/src/Components/Login/Login.js
@@ -1,54 +1,60 @@
-import {
-    BrowserRouter as Router,
-    Routes, Route, Link, useNavigate
-} from "react-router-dom"
-import sideimage from '../Assets/img-01.png'
+import './main.css'
+import image1 from './images/img-01.png'
+import './Login_v1/fonts/font-awesome-4.7.0/css/font-awesome.min.css'
 import { setLogin } from "../../Controllers/login"
 import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from "react"
 import { logInUser } from "../../Reducers/loggedInUserReducer"
-const Login = () => {
-    const navigate = useNavigate()
-    const redirect = () => {
-        navigate('/signup')
-    }
-    const dispatch = useDispatch();
-    const loggedInUser = useSelector(state => state.loggedInUser)
+const Login=()=>{
+    return(
+        <>
+        <div className="limiter">
+		<div className="container-login100">
+			<div className="wrap-login100">
+				<div className="login100-pic js-tilt" data-tilt>
+					<img src={image1} alt="IMG"/>
+				</div>
 
-    const login = async (event) => {
-        event.preventDefault();
-        const username = event.target.username.value;
-        const password = event.target.password.value;
-        const data = { username, password }
-        const res = await setLogin(data);
-        console.log("Clicked,", res)
-        window.localStorage.setItem('loggedInUser', JSON.stringify(res))
-        dispatch(logInUser(res))
-        console.log("U there?")
-        navigate('/')
-    }
+				<form className="login100-form validate-form">
+					<span className="login100-form-title">
+						Member Login
+					</span>
 
-    return (
+					<div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input className="input100" type="text" name="email" placeholder="Email"/>
+						<span className="focus-input100"></span>
+						<span className="symbol-input100">
+							<i className="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
 
-        <div className="loginMain">
-            <div className='loginWrapper'>
-                <div>
-                    <p>Hello Image</p>
-                    <img src={sideimage} alt='No Image' />
-                </div>
-                <div>
-                    <form onSubmit={login}>
-                        <input name="username" placeholder='username' />
-                        <br /><br />
-                        <input name="password" placeholder='password' type={'password'} />
-                        <br /><br />
-                        <button >Log In</button>
+					<div className="wrap-input100 validate-input" data-validate = "Password is required">
+						<input className="input100" type="password" name="pass" placeholder="Password"/>
+						<span className="focus-input100"></span>
+						<span className="symbol-input100">
+							<i className="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div className="container-login100-form-btn">
+						<button className="login100-form-btn">
+							Login
+						</button>
+					</div>
 
-                        <a onClick={redirect}>Sign Up</a>
-                    </form>
-                </div>
-            </div>
-        </div>
+					
+
+					<div className="text-center p-t-136">
+						<a className="txt2" href="#">
+							Create your Account
+							<i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+    </>
     )
 }
 export default Login;
